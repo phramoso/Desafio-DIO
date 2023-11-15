3d51cc3d5e0563b58d54e1364ced399a952e9cec
phramoso <phramosoliveira@gmail.com>
2023-11-14 23:12:14 -0300

    Desafio V1

---
+++ index.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Solicita o nome e a quantidade de experiência (XP) do herói
+rl.question("Digite o nome do herói: ", function(nomeHeroi) {
+    rl.question("Digite a quantidade de XP do herói: ", function(xpHeroi) {
+        xpHeroi = parseInt(xpHeroi);
+
+        // Estrutura de decisão para determinar o nível do herói com base na XP
+        var nivel;
+
+        if (xpHeroi < 1000) {
+            nivel = "Ferro";
+        } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+            nivel = "Bronze";
+        } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+            nivel = "Prata";
+        } else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+            nivel = "Ouro";
+        } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+            nivel = "Platina";
+        } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+            nivel = "Ascendente";
+        } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+
+        // Exibe a mensagem final
+        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+
+        rl.close();
+    });
+});
